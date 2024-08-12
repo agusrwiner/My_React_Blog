164c293ebbbd5626c41d59e78b3336450ff62a65
Agustin R. Winer <r.w.agustin@gmail.com>
2024-08-12 16:21:09 -0300

    "Added NavBar component and wrapped Routes with div elements in App component"

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -6,20 +6,26 @@ import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <HomePage /> } />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/articles" element={<ArticlesListPage />} />
-        <Route path="/articles/:articleId" element={<ArticlePage />} />
-      </Routes>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App

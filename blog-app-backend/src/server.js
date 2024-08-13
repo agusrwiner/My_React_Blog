d08320a8a380a57812afbfdfa8b0dff1eb43d008
Agustin R. Winer <r.w.agustin@gmail.com>
2024-08-13 14:30:34 -0300

    "Added comments property to articles and implemented API endpoint to post comments"

--- blog-app-backend/src/server.js
+++ blog-app-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
     {
         name: 'learn-react',
         upvotes: 0,
+        comments: [],
     },
     {
         name: 'learn-node',
         upvotes: 0,
+        comments: [],
     },
     {
         name: 'mongodb',
         upvotes: 0,
+        comments: [],
     }
 ]
 
@@ -25,6 +28,21 @@ app.put('/api/articles/:name/upvote',(req,res) => {
     if (article) {
         article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvotes!`);
+    } else {
+        res.send(`Article not found :(`);
+    }
+});
+
+app.post('/api/articles/:name/comments',(req,res) => {
+    const { postedBy,text } = req.body;
+    const {name} = req.params;
+
+    const article = articlesInfo.find(article => article.name === name);
+
+    if (article) {
+        article.comments.push( {postedBy, text} );
+        // res.send(`The ${article.name} article recieved a comment by ${postedBy}!`);
+        res.send( article.comments );
     } else {
         res.send(`Article not found :(`);
     }
